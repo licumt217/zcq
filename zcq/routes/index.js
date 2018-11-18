603b0fae83910b699b32e73d72e9205c979c996b
liqiang <qinag.li@tingjiandan.com>
2018-11-18 13:23:44 +0800

    成功部署服务器

--- zcq/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const Response=require('../config/response')
-
-router.get('/', function (req, res) {
-    res.send(Response.businessException("地址不正确，请勿直接访问根目录！"))
-})
-
-module.exports = router
